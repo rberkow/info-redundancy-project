c11f70641b421b28f4d8b3ce6a28777f8f212f74
KatieDuff <katherine.duff@mail.mcgill.ca>
2017-03-28 17:12:42 -0400

    updated run experiment
    *runs on all 3 error detection schemes 
    *runs on 5 different randomly generated messages, 1000 times for each

--- run_experiment.m
+++ run_experiment.m
@@ -1,41 +1,55 @@
 block_size = 4;
+gauss_BER = zeros(5,3);
+burst_BER = zeros(5,3);
+BER_gauss_before_decode = zeros(5,3);
+BER_burst_before_decode = zeros(5,3);
+gauss_BER_ratio = zeros(1,5);
+burst_BER_ratio = zeros(1,5);
 % Transmitter
-original_message = repmat([0 0 1 0 0 1 1 1], 1, 8);
-gauss_BER = zeros(1,2);
-burst_BER = zeros(1,2);
-BER_gauss_before_decode = zeros(1,2);
-BER_burst_before_decode = zeros(1,2);
+iter = 5;
 n = 1000;
-
-for i=1:n
-    encoded_message = two_d_parity_encoder(original_message, block_size);
-    encoded_message_TMR = bitwise_TMR_encoder(original_message);
-    % Error channel
-    encoded_message_with_gauss_error = gauss_error(encoded_message);
-    encoded_message_with_burst_error = burst_error(encoded_message, block_size*2);
-    encoded_message_with_gauss_error_TMR = gauss_error(encoded_message_TMR);
-    encoded_message_with_burst_error_TMR = burst_error(encoded_message_TMR, block_size*2);
-    % Check that error models are equivalent
-    BER_gauss_before_decode(1) = BER_gauss_before_decode(1) + error_analysis(noisy_to_bits(encoded_message_with_gauss_error), encoded_message);
-    BER_burst_before_decode(1) = BER_burst_before_decode(1) + error_analysis(noisy_to_bits(encoded_message_with_burst_error), encoded_message);
-    BER_gauss_before_decode(2) = BER_gauss_before_decode(2) + error_analysis(noisy_to_bits(encoded_message_with_gauss_error_TMR), encoded_message_TMR);
-    BER_burst_before_decode(2) = BER_burst_before_decode(2) + error_analysis(noisy_to_bits(encoded_message_with_burst_error_TMR), encoded_message_TMR);
-    % Receiver
-    decoded_message_gauss = two_d_parity_decoder(noisy_to_bits(encoded_message_with_gauss_error), block_size);
-    decoded_message_burst = two_d_parity_decoder(noisy_to_bits(encoded_message_with_burst_error), block_size);
-    decoded_message_gauss_TMR = bitwise_TMR_decoder(noisy_to_bits(encoded_message_with_gauss_error_TMR));
-    decoded_message_burst_TMR = bitwise_TMR_decoder(noisy_to_bits(encoded_message_with_burst_error_TMR));
-    gauss_BER(1) = gauss_BER(1) + error_analysis(original_message, decoded_message_gauss);
-    burst_BER(1) = burst_BER(1) + error_analysis(original_message, decoded_message_burst);
-    gauss_BER(2) = gauss_BER(2) + error_analysis(original_message, decoded_message_gauss_TMR);
-    burst_BER(2) = burst_BER(2) + error_analysis(original_message, decoded_message_burst_TMR);
+for j=1:iter
+    original_message = randi([0 1],1,64);
+    for i=1:n
+        encoded_message_2dparity = two_d_parity_encoder(original_message, block_size);
+        encoded_message_TMR = bitwise_TMR_encoder(original_message);
+        encoded_message_hamm = hamming_7_4_encoder(original_message);
+        % Error channel
+        encoded_message_2dparity_with_gauss_error = gauss_error(encoded_message_2dparity);
+        encoded_message_2dparity_with_burst_error = burst_error(encoded_message_2dparity, block_size*2);
+        encoded_message_TMR_with_gauss_error = gauss_error(encoded_message_TMR);
+        encoded_message_TMR_with_burst_error = burst_error(encoded_message_TMR, block_size*2);
+        encoded_message_hamm_with_gauss_error = gauss_error(encoded_message_hamm);
+        encoded_message_hamm_with_burst_error = burst_error(encoded_message_hamm, block_size*2);
+        % Check that error models are equivalent
+        BER_gauss_before_decode(j,1) = BER_gauss_before_decode(j,1) + error_analysis(noisy_to_bits(encoded_message_2dparity_with_gauss_error), encoded_message_2dparity);
+        BER_burst_before_decode(j,1) = BER_burst_before_decode(j,1) + error_analysis(noisy_to_bits(encoded_message_2dparity_with_burst_error), encoded_message_2dparity);
+        BER_gauss_before_decode(j,2) = BER_gauss_before_decode(j,2) + error_analysis(noisy_to_bits(encoded_message_TMR_with_gauss_error), encoded_message_TMR);
+        BER_burst_before_decode(j,2) = BER_burst_before_decode(j,2) + error_analysis(noisy_to_bits(encoded_message_TMR_with_burst_error), encoded_message_TMR);
+        BER_gauss_before_decode(j,3) = BER_gauss_before_decode(j,3) + error_analysis(noisy_to_bits(encoded_message_hamm_with_gauss_error), encoded_message_hamm);
+        BER_burst_before_decode(j,3) = BER_burst_before_decode(j,3) + error_analysis(noisy_to_bits(encoded_message_hamm_with_burst_error), encoded_message_hamm);
+        % Receiver
+        decoded_message_gauss_2dparity = two_d_parity_decoder(noisy_to_bits(encoded_message_2dparity_with_gauss_error), block_size);
+        decoded_message_burst_2dparity = two_d_parity_decoder(noisy_to_bits(encoded_message_2dparity_with_burst_error), block_size);
+        decoded_message_gauss_TMR = bitwise_TMR_decoder(noisy_to_bits(encoded_message_TMR_with_gauss_error));
+        decoded_message_burst_TMR = bitwise_TMR_decoder(noisy_to_bits(encoded_message_TMR_with_burst_error));
+        decoded_message_gauss_hamm = hamming_7_4_decoder(noisy_to_bits(encoded_message_hamm_with_gauss_error));
+        decoded_message_burst_hamm = hamming_7_4_decoder(noisy_to_bits(encoded_message_hamm_with_burst_error));
+        gauss_BER(j,1) = gauss_BER(j,1) + error_analysis(original_message, decoded_message_gauss_2dparity);
+        burst_BER(j,1) = burst_BER(j,1) + error_analysis(original_message, decoded_message_burst_2dparity);
+        gauss_BER(j,2) = gauss_BER(j,2) + error_analysis(original_message, decoded_message_gauss_TMR);
+        burst_BER(j,2) = burst_BER(j,2) + error_analysis(original_message, decoded_message_burst_TMR);
+        gauss_BER(j,3) = gauss_BER(j,3) + error_analysis(original_message, decoded_message_gauss_hamm);
+        burst_BER(j,3) = burst_BER(j,3) + error_analysis(original_message, decoded_message_burst_hamm);
+    end
 end
-
 BER_gauss_before_decode = BER_gauss_before_decode/n;
 BER_burst_before_decode = BER_burst_before_decode/n;
 
 gauss_BER = (gauss_BER/n);
 burst_BER = (burst_BER/n);
 
-gauss_BER_ratio = rdivide(gauss_BER,BER_gauss_before_decode)
-burst_BER_ratio = rdivide(burst_BER,BER_burst_before_decode)
+gauss_BER_ratio = rdivide(gauss_BER,BER_gauss_before_decode);
+burst_BER_ratio = rdivide(burst_BER,BER_burst_before_decode);
+mean_gauss_BER_ratio = mean(gauss_BER_ratio)
+mean_burst_BER_ratio = mean(burst_BER_ratio)

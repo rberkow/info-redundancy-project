7fd0187f0a10ef0cfea70336f8d4c5c848e71f96
Ruth Berkow <ruth.berkow@mail.mcgill.ca>
2017-03-30 11:35:44 -0400

    Adding timing to experiment

--- run_experiment.m
+++ run_experiment.m
@@ -8,13 +8,23 @@ burst_BER_ratio = zeros(1,5);
 % Transmitter
 iter = 5;
 n = 1000;
+time_2d = [0 0];
+time_tmr = [0 0];
+time_hamm = [0 0];
 for j=1:iter
+    % Generate message and encode it
     original_message = randi([0 1],1,64);
+    tic;
+    encoded_message_2dparity = two_d_parity_encoder(original_message, block_size);
+    time_2d(1) = time_2d(1) + toc; 
+    tic;
+    encoded_message_TMR = bitwise_TMR_encoder(original_message);
+    time_tmr(1) = time_tmr(1) + toc;
+    tic;
+    encoded_message_hamm = hamming_7_4_encoder(original_message);
+    time_hamm(1) = time_hamm(1) + toc;
     for i=1:n
-        encoded_message_2dparity = two_d_parity_encoder(original_message, block_size);
-        encoded_message_TMR = bitwise_TMR_encoder(original_message);
-        encoded_message_hamm = hamming_7_4_encoder(original_message);
-        % Error channel
+        % Error channels
         encoded_message_2dparity_with_gauss_error = gauss_error(encoded_message_2dparity);
         encoded_message_2dparity_with_burst_error = burst_error(encoded_message_2dparity, block_size*2);
         encoded_message_TMR_with_gauss_error = gauss_error(encoded_message_TMR);
@@ -29,11 +39,17 @@ for j=1:iter
         BER_gauss_before_decode(j,3) = BER_gauss_before_decode(j,3) + error_analysis(noisy_to_bits(encoded_message_hamm_with_gauss_error), encoded_message_hamm);
         BER_burst_before_decode(j,3) = BER_burst_before_decode(j,3) + error_analysis(noisy_to_bits(encoded_message_hamm_with_burst_error), encoded_message_hamm);
         % Receiver
+        tic;
         decoded_message_gauss_2dparity = two_d_parity_decoder(noisy_to_bits(encoded_message_2dparity_with_gauss_error), block_size);
+        time_2d(2) = time_2d(2) + toc;
         decoded_message_burst_2dparity = two_d_parity_decoder(noisy_to_bits(encoded_message_2dparity_with_burst_error), block_size);
+        tic;
         decoded_message_gauss_TMR = bitwise_TMR_decoder(noisy_to_bits(encoded_message_TMR_with_gauss_error));
+        time_tmr(2) = time_tmr(2) + toc;
         decoded_message_burst_TMR = bitwise_TMR_decoder(noisy_to_bits(encoded_message_TMR_with_burst_error));
+        tic;
         decoded_message_gauss_hamm = hamming_7_4_decoder(noisy_to_bits(encoded_message_hamm_with_gauss_error));
+        time_hamm(2) = time_hamm(2) + toc;
         decoded_message_burst_hamm = hamming_7_4_decoder(noisy_to_bits(encoded_message_hamm_with_burst_error));
         gauss_BER(j,1) = gauss_BER(j,1) + error_analysis(original_message, decoded_message_gauss_2dparity);
         burst_BER(j,1) = burst_BER(j,1) + error_analysis(original_message, decoded_message_burst_2dparity);
@@ -53,3 +69,7 @@ gauss_BER_ratio = rdivide(gauss_BER,BER_gauss_before_decode);
 burst_BER_ratio = rdivide(burst_BER,BER_burst_before_decode);
 mean_gauss_BER_ratio = mean(gauss_BER_ratio)
 mean_burst_BER_ratio = mean(burst_BER_ratio)
+
+time_2d = sum(time_2d./[iter, n]);
+time_tmr = sum(time_tmr./[iter, n]);
+time_hamm = sum(time_hamm./[iter, n]);
